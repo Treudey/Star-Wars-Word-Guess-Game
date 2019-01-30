9ad16ea2d7e259e28fb2f49207f760410ba36335
Treudey <alextreude@hotmail.com>
2019-01-30 11:53:47 -0500

    made some small style changes and added a plan in my js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+/* 
+* create an algorithm
+* a plan that solves a problem
+* problem? guess the letter
+
+* create a list of defined letters that can be guessed
+* pick a random letter from the list
+* store that random letter in a variable
+
+* define a var that stores total number of guesses
+
+* have user enter a key
+* listen to a key event
+* check if key pressed matches random letter
+* increase score by one
+* else
+* decrease total number of guesses by one
+
+* if total number of guesses = 0
+* then score = score - 1
+* reset guesses to be max number of guesses
+* choose another randome letter
+* repeat game again
+*/
+
