19daebb5aae95c30639b2d30afbf4b22f75e61f8
Treudey <alextreude@hotmail.com>
2019-03-26 18:20:18 -0400

    Moved all code into game object and runs simply with game.init

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,12 +15,16 @@ var game = {
     guessword: document.querySelector("#guessword"),
     guessedLetters: document.querySelector("#guessed-letters"),
     remainingGuesses: document.querySelector("#remaining-guesses"),
+    randomNum: 0,
+    usedNums: [],
+    selectedWord: '',
+
 
     // METHODS
     // ============================================================================
     displayBlankWord: function() {
         // prints the blank spaces for the random word that will change to letters as guesses are made
-        for (var i = 0; i < selectedWord.length; i++) {
+        for (var i = 0; i < this.selectedWord.length; i++) {
             var newElement = document.createElement("span");
             newElement.setAttribute("id", "char" + i);
             newElement.innerHTML = "_ ";
@@ -35,18 +39,20 @@ var game = {
     },
     isInWord: function(string, char) {      // checks if a given character is in a given string
          return string.indexOf(char) > -1;
-       
     },
     getUniqueNum: function() {
         while (true) {
-            randomNum = Math.floor(Math.random() * this.words.length);
-            if (!this.isInArray(usedNums, randomNum)) {        // checks that random number has not been used before
-                selectedWord = this.words[randomNum];
-                usedNums.push(randomNum);                  // adds it to the list of used numbers
+            this.randomNum = Math.floor(Math.random() * this.words.length);
+            if (!this.isInArray(this.usedNums, this.randomNum)) {        // checks that random number has not been used before
+                this.selectedWord = this.words[this.randomNum];
+                this.usedNums.push(this.randomNum);                  // adds it to the list of used numbers
                 return false
-            } else if (this.words.length === usedNums.length && usedNums[usedNums.length-1] !== randomNum) {  // if all the words have been used and the random number is not the same one just used
-                usedNums = [randomNum];                    // resets usedNum to include the new random number
-                selectedWord = this.words[randomNum];
+            // if all the words have been used and the random number is not the same one just used
+            } else if (this.words.length === this.usedNums.length && 
+                        this.usedNums[this.usedNums.length-1] !== this.randomNum) {  
+                // resets usedNum to include the new random number
+                this.usedNums = [this.randomNum];                    
+                this.selectedWord = this.words[this.randomNum];
                 return false;
             }
         }
@@ -62,53 +68,49 @@ var game = {
         this.guessedLetters.innerHTML = this.incorrectLetters;
 
         this.displayBlankWord();
+        
+    },
+    processGuess: function(event) {
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            if (this.isInWord(this.selectedWord, event.key) && !this.isInArray(this.correctLetters, event.key)) { //  if letter is part of word and if it hasn't been guessed before
+            for (var i = 0; i < this.selectedWord.length; i++) {
+                var currentLetter = this.selectedWord.charAt(i);
+                if (currentLetter === event.key) {
+                    var letterPosition = document.querySelector("#char" + i);
+                    letterPosition.innerHTML = currentLetter; // prints all the matching characters that are in the word
+                    letterPosition.style.cssText = "font-family: 'Star Wars', Arial, Helvetica, sans-serif";
+                    this.correctLetters.push(event.key);         // adds it to the correct guesses list
+                }
+            }
+            } else if (this.isInArray(this.correctLetters, event.key)) {   // if a correct letter is pressed again it will not be printed again
+                return;
+            } else if (!this.isInArray(this.incorrectLetters, event.key)) { // if it hasn't been guessed it will add it to incorrectLetters
+                this.incorrectLetters.push(event.key);
+                this.guessedLetters.innerHTML = this.incorrectLetters;
+                this.totalGuesses -= 1;
+            } 
+
+            if (this.totalGuesses === 0) {     // losing condition
+                this.reset();
+            } else if (this.correctLetters.length === this.selectedWord.length) {    // winning condition
+                this.wins++;                                                        // increases wins by one and prints the updated value
+                document.getElementById("number-of-wins").innerHTML = this.wins;
+                document.getElementById("sw-img").src = this.imgList[this.randomNum];      // displays relevant picture from the list of image files
+                var audio = new Audio(this.audioList[this.randomNum]);                     // plays the relevant sound byte from the list of audio files
+                audio.play();                                                             
+                this.reset();
+            }
+            // updates number of guesses to the page
+            this.remainingGuesses.innerHTML = this.totalGuesses;    
+            // prints to screen the total amount of guessesguesses
+        }
+    },
+    init: function() {
+        this.reset();
+        document.addEventListener("keydown", this.processGuess.bind(this));
     }
 };
 
-// VARIABLES
-// ===============================================================================
-var randomNum = Math.floor(Math.random() * game.words.length); // couldn't figure out how to put these into the object without it recalculating the random number every time
-var usedNums = [randomNum];
-var selectedWord = game.words[randomNum];
-
 // MAIN PROCESS
 // ============================================================================
-game.displayBlankWord();
-
-document.addEventListener("keydown", function(event) {
-    console.log(event.keyCode);
-    debugger;
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-        if (game.isInWord(selectedWord, event.key) && !game.isInArray(game.correctLetters, event.key)) { //  if letter is part of word and if it hasn't been guessed before
-        for (var i = 0; i < selectedWord.length; i++) {
-            var currentLetter = selectedWord.charAt(i);
-            if (currentLetter === event.key) {
-                var letterPosition = document.querySelector("#char" + i);
-                letterPosition.innerHTML = currentLetter; // prints all the matching characters that are in the word
-                letterPosition.style.cssText = "font-family: 'Star Wars', Arial, Helvetica, sans-serif";
-                game.correctLetters.push(event.key);         // adds it to the correct guesses list
-            }
-        }
-        } else if (game.isInArray(game.correctLetters, event.key)) {   // if a correct letter is pressed again it will not be printed again
-            return;
-        } else if (!game.isInArray(game.incorrectLetters, event.key)) { // if it hasn't been guessed it will add it to incorrectLetters
-            game.incorrectLetters.push(event.key);
-            game.guessedLetters.innerHTML = game.incorrectLetters;
-            game.totalGuesses -= 1;
-        } 
-
-        if (game.totalGuesses === 0) {     // losing condition
-            game.reset();
-        } else if (game.correctLetters.length === selectedWord.length) {    // winning condition
-            game.wins++;                                                        // increases wins by one and prints the updated value
-            document.getElementById("number-of-wins").innerHTML = game.wins;
-            document.getElementById("sw-img").src = game.imgList[randomNum];      // displays relevant picture from the list of image files
-            var audio = new Audio(game.audioList[randomNum]);                     // plays the relevant sound byte from the list of audio files
-            audio.play();                                                             
-            game.reset();
-        }
-        // updates number of guesses to the page
-        game.remainingGuesses.innerHTML = game.totalGuesses;    // prints to screen the total amount of guessesguesses
-
-    }
-});
+game.init();
